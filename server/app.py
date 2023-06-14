f36118d873d88b1776585cb45b8d83d82158c122
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-06-14 10:39:22 -0400

    alina-solution

--- server/app.py
+++ server/app.py
@@ -19,15 +19,55 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    
+    return make_response(
+        f'''
+            <ul style="font-size: 40px">
+                <li>ID: {animal.id}</li>
+                <li>Name: {animal.name}</li>
+                <li>Species: {animal.species}</li>
+                <li>Zookeper: {animal.zookeeper.name}</li>
+                <li>Environment: {animal.enclosure.environment}</li>
+            </ul>
+        '''
+    )
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    
+    response_body = f'''
+            <ul style="font-size: 40px">
+                <li>ID: {zookeeper.id}</li>
+                <li>Name: {zookeeper.name}</li>
+                <li>Birthday: {zookeeper.birthday}</li>
+            </ul>
+            <p style="font-size: 40px">Animals:</p>
+        '''
+    
+    for animal in zookeeper.animals:
+        response_body += f'<li style="font-size: 40px">{animal.name}</li>'
+
+    return make_response(response_body)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    
+    response_body = f'''
+            <ul style="font-size: 40px">
+                <li>ID: {enclosure.id}</li>
+                <li>Environment: {enclosure.environment}</li>
+                <li>Open to visitors: {enclosure.open_to_visitors}</li>
+            </ul>
+            <p style="font-size: 40px">Animals:</p>
+        '''
+    
+    for animal in enclosure.animals:
+        response_body += f'<li style="font-size: 40px">{animal.name}</li>'
+        
+    return make_response(response_body)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,23 +1,45 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+# from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+# metadata = MetaData(naming_convention={
+#      "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', backref='zookeeper')
+
+    def __repr__(self):
+        return f'Zookeeper(id: {self.id}, name: {self.name})'
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __repr__(self):
+        return f'Enclosure(id: {self.id}, environment: {self.environment})'
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __repr__(self):
+        return f'Animal(id: {self.id}, name: {self.name}, species: {self.species})'
